30818cab3e4eb6aae84b182724c5dc9e77e2a154
Ananth Suryanarayana <anantha@juniper.net>
2016-03-05 15:37:06 -0800

    tests without bgp pass

--- src/bgp/test/graceful_restart_test.cc
+++ src/bgp/test/graceful_restart_test.cc
@@ -44,7 +44,7 @@ using ::testing::Combine;
 static vector<int>  n_instances = boost::assign::list_of(8);
 static vector<int>  n_routes    = boost::assign::list_of(8);
 static vector<int>  n_agents    = boost::assign::list_of(8);
-static vector<int>  n_peers     = boost::assign::list_of(8);
+static vector<int>  n_peers     = boost::assign::list_of(0);
 static vector<int>  n_targets   = boost::assign::list_of(1);
 
 static char **gargv;
@@ -1808,7 +1808,7 @@ TEST_P(GracefulRestartTest, GracefulRestart_Flap_Some_3_2) {
         n_flipped_agents_[i].send_eor = false;
     }
 
-    for (size_t i = 0; i < xmpp_agents_.size()/2; i++) {
+    for (size_t i = 0; i < bgp_peers_.size()/2; i++) {
         BgpPeerTest *peer = bgp_peers_[i];
         vector<int> instance_ids = vector<int>();
         vector<int> nroutes = vector<int>();
