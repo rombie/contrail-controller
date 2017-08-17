547a97ef4fca170897a8798d3ae6aaf2d71ca654
Ananth Suryanarayana <anantha@juniper.net>
2017-08-17 13:41:10 -0700

    cp
    Change-Id: Ifb0c05fb270d0b275a7fcf491c2b57ebaf1af99b

--- src/bgp/bgp_table.cc
+++ src/bgp/bgp_table.cc
@@ -434,7 +434,7 @@ bool BgpTable::InputCommon(DBTablePartBase *root, BgpRoute *rt, BgpPath *path,
         new_path = new BgpPath(
             peer, path_id, BgpPath::BGP_XMPP, attrs, flags, label, l3_label);
         if (new_path->NeedsResolution()) {
-            Address::Family family = path->GetAttr()->nexthop_family();
+            Address::Family family = new_path->GetAttr()->nexthop_family();
             BgpTable *table = rtinstance_->GetTable(family);
             path_resolver_->StartPathResolution(rt, new_path, table);
         }
