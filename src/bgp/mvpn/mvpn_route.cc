c704614b20a519d353d4965e58d564356973a7ad
Ananth Suryanarayana <anantha@juniper.net>
2017-09-04 02:34:23 -0700

    cp
    Change-Id: I955abd4e39fbc7987f040052acb7a904cb964d2d

--- src/bgp/mvpn/mvpn_route.cc
+++ src/bgp/mvpn/mvpn_route.cc
@@ -15,7 +15,7 @@ using std::copy;
 using std::string;
 using std::vector;
 
-MvpnPrefix::MvpnPrefix() : type_(MvpnPrefix::Unspecified), ip_prefixlen_(0), 
+MvpnPrefix::MvpnPrefix() : type_(MvpnPrefix::Unspecified), ip_prefixlen_(0),
         asn_(0) {
 }
 
