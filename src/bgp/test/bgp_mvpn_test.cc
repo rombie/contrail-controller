781cb6bda71868218b29173148a603dd6d22a82c
Ananth Suryanarayana <anantha@juniper.net>
2018-01-05 15:45:01 -0800

    cp
    Change-Id: I7a464af14da946aebefab2f893add3a4cb06ad8f

--- src/bgp/test/bgp_mvpn_test.cc
+++ src/bgp/test/bgp_mvpn_test.cc
@@ -933,9 +933,9 @@ TEST_P(BgpMvpnTest, Type3_SPMSI_With_ErmVpnRoute) {
     for (size_t i = 1; i <= instances_set_count_; i++) {
         for (size_t j = 1; j <= groups_count_; j++) {
             tbb::mutex::scoped_lock lock(pmsi_params_mutex);
-            ermvpn_rt[(i-1)*instances_set_count_+(j-1)] = NULL;
+            ermvpn_rt[(i-1)*groups_count_+(j-1)] = NULL;
             PMSIParams pmsi(PMSIParams(10, "1.2.3.4", "gre",
-                            &ermvpn_rt[(i-1)*instances_set_count_+(j-1)]));
+                            &ermvpn_rt[(i-1)*groups_count_+(j-1)]));
             pmsi_params.insert(make_pair(sg(i, j), pmsi));
             lock.release();
             AddMvpnRoute(master_, prefix3(i,j), getRouteTarget(i, "1"));
@@ -964,7 +964,7 @@ TEST_P(BgpMvpnTest, Type3_SPMSI_With_ErmVpnRoute) {
                 AddErmVpnRoute(fabric_ermvpn_[i-1], ermvpn_prefix(i, j),
                                "target:127.0.0.1:1100");
             tbb::mutex::scoped_lock lock(pmsi_params_mutex);
-            ermvpn_rt[(i-1)*instances_set_count_+(j-1)] = rt;
+            ermvpn_rt[(i-1)*groups_count_+(j-1)] = rt;
         }
     }
 
