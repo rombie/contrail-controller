bd8b3c6879b7471bad85d5bf2b428324fc7841aa
Ananth Suryanarayana <anantha@juniper.net>
2019-08-22 15:53:08 -0700

    cat fixes
    Change-Id: I75e3dabc85b4139b397a51ee4742d7100d340944

--- src/bgp/test/cat/cat.go
+++ src/bgp/test/cat/cat.go
@@ -1,32 +1,28 @@
 package cat
 
 import (
-	"bufio"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"os/signal"
-	"strconv"
-	"strings"
-	"syscall"
-	"time"
+    "errors"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "path/filepath"
+    "strings"
+    "syscall"
+    "time"
 
-	"cat/agent"
-	"cat/controlnode"
-	"cat/sut"
+    "cat/agent"
+    "cat/controlnode"
+    "cat/sut"
 )
 
 // CAT is a contrail automated test.
 type CAT struct {
-	sut           component.SUT
-	PauseAfterRun bool
+    sut           sut.Component
+    PauseAfterRun bool
 
-	ControlNodes []*controlNode.ControlNode
-	Agents       []*agent.Agent
+    ControlNodes []*controlnode.ControlNode
+    Agents       []*agent.Agent
 }
 
 // Timestamp format for logfiles.
@@ -34,97 +30,109 @@ const timestamp = "20060102_150405"
 
 // New creates an initialized CAT instance.
 func New() (*CAT, error) {
-	c := &CAT{}
-	now := time.Now()
+    c := &CAT{}
+    now := time.Now()
 
-	c.sut.LogDir = filepath.Join("/tmp/CAT", os.Getenv("USER"), now.Format(timestamp))
-	if err := os.MkdirsAll(c.LogDir); err != nil {
-		return nil, fmt.Errorf("failed to create logdir %q :%v", c.LogDir, err)
-	}
-	fmt.Println("Logs are in " + c.LogDir)
-	c.setHostIP()
+    c.sut.LogDir = filepath.Join("/tmp/CAT", os.Getenv("USER"),
+                                 now.Format(timestamp))
+    if err := os.MkdirAll(c.sut.LogDir, 0700); err != nil {
+        return nil, fmt.Errorf("failed to create logdir %q :%v",
+                               c.sut.LogDir, err)
+    }
+    fmt.Println("Logs are in " + c.sut.LogDir)
+    c.setHostIP()
 
-	if err := os.MkdirAll(c.sut.ReportDir); err != nil {
-		return nil, fmt.Errorf("failed to make report directory: %v", err)
-	}
-	return c
+    err := os.MkdirAll(c.sut.Manager.ReportDir, 0700)
+    if err != nil {
+        return nil, fmt.Errorf("failed to make report directory: %v", err)
+    }
+    return c, err
 }
 
 func (c *CAT) RedirectStdOuterr() {
-	/*
-	   old := os.dup(1)
-	   os.close(1)
-	   f = self.log_files + "/" + self.Name + ".log"
-	   open(f, 'w')
-	   os.open(f, os.O_WRONLY)
+    /*
+       old := os.dup(1)
+       os.close(1)
+       f = self.log_files + "/" + self.Name + ".log"
+       open(f, 'w')
+       os.open(f, os.O_WRONLY)
 
-	   old = os.dup(2)
-	   os.close(2)
-	   f = self.log_files + "/" + self.Name + ".log"
-	   open(f, 'w')
-	   os.open(f, os.O_WRONLY)
-	*/
+       old = os.dup(2)
+       os.close(2)
+       f = self.log_files + "/" + self.Name + ".log"
+       open(f, 'w')
+       os.open(f, os.O_WRONLY)
+    */
 }
 
 // Teardown stops all components and closes down the CAT instance.
 func (c *CAT) Teardown() {
-	if c.PauseAfterRun {
-		c.Pause()
-	}
-	for _, cn := range c.ControlNodes {
-		cn.Stop()
-	}
-	for _, a := range c.Agents {
-		a.Stop()
-	}
-	os.Remove(c.LogDir)
+    if c.PauseAfterRun {
+        c.Pause()
+    }
+    for _, cn := range c.ControlNodes {
+        cn.Component.Stop(syscall.SIGTERM)
+    }
+    for _, a := range c.Agents {
+        a.Component.Stop(syscall.SIGTERM)
+    }
+    os.Remove(c.sut.LogDir)
 }
 
 func (c *CAT) Pause() {
-	cmd := exec.Command("/usr/bin/python")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(2600 * time.Second)
+    cmd := exec.Command("/usr/bin/python")
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    err := cmd.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+    time.Sleep(2600 * time.Second)
 }
 
-func (c *CAT) hostIP() error {
-	cmd := exec.Command("hostname", "-i")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatal("Cannot find host ip address")
-		return "", err
-	}
+func (c *CAT) setHostIP() error {
+    cmd := exec.Command("hostname", "-i")
+    out, err := cmd.CombinedOutput()
+    if err != nil {
+        log.Fatal("Cannot find host ip address")
+        return err
+    }
 
-	ips := strings.Split(string(out), " ")
-	for _, ip := range ips {
-		if !strings.HasPrefix(ip, "127.") {
-			c.sut.IP = strings.Trim(ip, "\n")
-			return nil
-		}
-	}
-	return "", errors.New("Cannot retrieve host ip address")
+    ips := strings.Split(string(out), " ")
+    for _, ip := range ips {
+        if !strings.HasPrefix(ip, "127.") {
+            c.sut.Manager.IP = strings.Trim(ip, "\n")
+            return nil
+        }
+    }
+    return errors.New("Cannot retrieve host ip address")
 }
 
-func (c *CAT) AddAgent(test string, name string, nodes []*ControlNode) (*Agent, err) {
-	agent, err := agent.New(test, name, c.SUT, nodes)
-	if err != nil {
-		return nil, fmt.Errorf("failed create agent: %v", err)
-	}
-	c.Agents = append(self.Agents, agent)
-	return agent, nil
+func (c *CAT) AddAgent(test string, name string,
+              control_nodes []*controlnode.ControlNode) (*agent.Agent, error) {
+    var xmpp_ports []int
+    for _, control_node := range control_nodes {
+        xmpp_ports = append(xmpp_ports, control_node.Config.XMPPPort)
+    }
+    agent, err := agent.New(c.sut.Manager, name, test, xmpp_ports)
+    if err != nil {
+        return nil, fmt.Errorf("failed create agent: %v", err)
+    }
+    c.Agents = append(c.Agents, agent)
+    return agent, nil
 }
 
-func (c *CAT) AddControlNode(test string, name string, port int) *ControlNode {
-	cn := controlnode.New(test, name, self)
-	con.Verbose = self.Verbose
-	con.HttpPort = port
-	con.Pid = con.Start()
-	con.ReadPortNumbers()
-	self.ControlNodes = append(self.ControlNodes, con)
-	return con
+func (c *CAT) AddControlNode(test string, name string,
+                             port int) (*controlnode.ControlNode, error) {
+    cn, err := controlnode.New(c.sut.Manager, name, test)
+    if err != nil {
+        return nil, fmt.Errorf("failed to create control-node: %v", err)
+    }
+    cn.Verbose = c.sut.Manager.Verbose
+    cn.Config.HTTPPort = port
+    cn.Config.Pid = cn.Start()
+    cn.ReadPortNumbers()
+    c.ControlNodes = append(c.ControlNodes, cn)
+    return cn, nil
 }
--- src/bgp/test/cat/test/basic_test.go
+++ src/bgp/test/cat/test/basic_test.go
@@ -1,120 +1,90 @@
 package main
 
 import (
-	"os"
-	"strconv"
-	"testing"
+    "strconv"
+    "testing"
+    "cat"
+    "cat/agent"
+    "cat/controlnode"
 )
 
+func createControlNodseAndAgents(t *testing.T, test string, nc,
+                                 na int) (*cat.CAT,
+        []*controlnode.ControlNode, []*agent.Agent, error) {
+    ct, err := cat.New()
+    if err != nil {
+        t.Fail()
+    }
+    control_nodes := []*controlnode.ControlNode{}
+    agents := []*agent.Agent{}
+
+    for c := 0; c < nc; c++ {
+        cn, err := ct.AddControlNode(test, "control-node" + strconv.Itoa(c), 0)
+        if err != nil {
+            return ct, control_nodes, agents, err
+        }
+        control_nodes = append(control_nodes, cn)
+    }
+
+    for a := 0; a < na; a++ {
+        ag, err := ct.AddAgent(test, "agent1", control_nodes)
+        if err != nil {
+            return ct, control_nodes, agents, err
+        }
+        agents = append(agents, ag)
+    }
+    verifyControlNodseAndAgents(t, control_nodes, agents)
+    return ct, control_nodes, agents, err
+}
+
+func verifyControlNodseAndAgents(t *testing.T,
+                                 control_nodes []*controlnode.ControlNode,
+                                 agents[]*agent.Agent) {
+    for c := range control_nodes {
+        if !control_nodes[c].CheckXmppConnections(agents, 30, 1) {
+            t.Fail()
+        }
+    }
+}
+
 func TestSingleControlNodeSingleAgent(t *testing.T) {
-	name := "TestSingleControlNodeSingleAgent"
-	cat := new(CAT).Initialize()
-	c1 := cat.AddControlNode(name, "control-node1", 0)
-	agent := cat.AddAgent(name, "agent1", []*ControlNode{c1})
-	agents := []*Agent{agent}
-	if !c1.CheckXmppConnections(agents, 30, 1) {
-		t.Fail()
-	}
-	cat.CleanUp()
+    cat, _, _, _ := createControlNodseAndAgents(
+        t, "TestSingleControlNodeSingleAgent", 1, 1)
+    cat.Teardown()
 }
 
 func TestSingleControlNodeMultipleAgent(t *testing.T) {
-	name := "TestSingleControlNodeMultipleAgent"
-	cat := new(CAT).Initialize()
-	c1 := cat.AddControlNode(name, "control-node1", 0)
-	agents := []*Agent{}
-	for i := 1; i <= 2; i++ {
-		agents = append(agents,
-			cat.AddAgent(name, "agent"+strconv.Itoa(i), []*ControlNode{c1}))
-	}
-	if !c1.CheckXmppConnections(agents, 30, 1) {
-		t.Fail()
-	}
-	cat.CleanUp()
+    cat, _, _, _ := createControlNodseAndAgents(
+        t, "TestSingleControlNodeMultipleAgent",1,3)
+    cat.Teardown()
 }
 
 func TestMultipleControlNodeSingleAgent(t *testing.T) {
-	name := "TestMultipleControlNodeSingleAgent"
-	cat := new(CAT).Initialize()
-	control_nodes := []*ControlNode{}
-	for i := 1; i <= 2; i++ {
-		control_nodes = append(control_nodes,
-			cat.AddControlNode(name, "control-node"+strconv.Itoa(i), 0))
-	}
-	agent := cat.AddAgent(name, "agent1", control_nodes)
-	agents := []*Agent{agent}
-	for i := 1; i <= 2; i++ {
-		if !control_nodes[i-1].CheckXmppConnections(agents, 30, 1) {
-			t.Fail()
-		}
-	}
-	cat.CleanUp()
+    cat, _, _, _ := createControlNodseAndAgents(
+        t, "TestMultipleControlNodeSingleAgent", 3, 1)
+    cat.Teardown()
 }
 
 func TestMultipleControlNodeMultipleAgent(t *testing.T) {
-	name := "TestMultipleControlNodeSingleAgent"
-	os.Chdir("controller/src/bgp/test/cat/lib")
-	cat := new(CAT).Initialize()
-	control_nodes := []*ControlNode{}
-	for i := 1; i <= 2; i++ {
-		control_nodes = append(control_nodes,
-			cat.AddControlNode(name, "control-node"+strconv.Itoa(i), 0))
-	}
-	agents := []*Agent{}
-	for i := 1; i <= 2; i++ {
-		agents = append(agents,
-			cat.AddAgent(name, "agent"+strconv.Itoa(i), control_nodes))
-	}
-	for i := 1; i <= 2; i++ {
-		if !control_nodes[i-1].CheckXmppConnections(agents, 30, 1) {
-			t.Fail()
-		}
-	}
-	cat.CleanUp()
+    cat, _, _, _ := createControlNodseAndAgents(
+        t, "TestMultipleControlNodeMultipleAgent", 3, 3)
+    cat.Teardown()
 }
 
 func TestSingleControlNodeRestart(t *testing.T) {
-	name := "TestSingleControlNodeRestart"
-	cat := new(CAT).Initialize()
-	c1 := cat.AddControlNode(name, "control-node1", 0)
-	agent := cat.AddAgent(name, "agent1", []*ControlNode{c1})
-	agents := []*Agent{agent}
-	if !c1.CheckXmppConnections(agents, 30, 1) {
-		t.Fail()
-	}
-	c1.Restart()
-	if !c1.CheckXmppConnections(agents, 30, 1) {
-		t.Fail()
-	}
-	cat.CleanUp()
+    cat, control_nodes, agents, _ := createControlNodseAndAgents(
+        t, "TestSingleControlNodeRestart", 1, 1)
+    control_nodes[0].Restart()
+    verifyControlNodseAndAgents(t, control_nodes, agents)
+    cat.Teardown()
 }
 
 func TestMultipleControlNodeRestart(t *testing.T) {
-	name := "TestMultipleControlNodeRestart"
-	os.Chdir("controller/src/bgp/test/cat/lib")
-	cat := new(CAT).Initialize()
-	control_nodes := []*ControlNode{}
-	for i := 1; i <= 2; i++ {
-		control_nodes = append(control_nodes,
-			cat.AddControlNode(name, "control-node"+strconv.Itoa(i), 0))
-	}
-	agents := []*Agent{}
-	for i := 1; i <= 2; i++ {
-		agents = append(agents,
-			cat.AddAgent(name, "agent"+strconv.Itoa(i), control_nodes))
-	}
-	for i := 1; i <= 2; i++ {
-		if !control_nodes[i-1].CheckXmppConnections(agents, 30, 1) {
-			t.Fail()
-		}
-	}
-	for i := 1; i <= 2; i++ {
-		control_nodes[i-1].Restart()
-	}
-	for i := 1; i <= 2; i++ {
-		if !control_nodes[i-1].CheckXmppConnections(agents, 30, 1) {
-			t.Fail()
-		}
-	}
-	cat.CleanUp()
+    cat, control_nodes, agents, _ := createControlNodseAndAgents(
+        t, "TestMultipleControlNodeRestart", 2, 3)
+    for c := range control_nodes {
+        control_nodes[c].Restart()
+    }
+    verifyControlNodseAndAgents(t, control_nodes, agents)
+    cat.Teardown()
 }
